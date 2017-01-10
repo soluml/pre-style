90e0f8c1f26838935ef3f9ea972d6a2f751a3987
Ben Solum <benjamin@soluml.com>
2017-01-10 12:35:48 -0600

    Allow adapter to be in config or node module.  Updated CSSNano config.

--- PreStyleConfig.js
+++ PreStyleConfig.js
@@ -1,13 +1,21 @@
+const Sass = require('node-sass');
+
 module.exports = {
   // Final destination of output
   cssDest: './src/client/css/_atomic.css',
 
-  // Adapter that processes our syntax. This file should return a process.
-  adapter: 'node-sass',
+  // Adapter that processes our syntax. This file or function should return a promise.
+  adapter(data) {
+    try {
+      return Promise.resolve(Sass.renderSync({ data, outputStyle: 'compact' }).css);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  },
 
   /*
     Files to be prepended for each bit of CSS.
     Should be variables and mixins *ONLY* otherwise you'll really blaot your StyleSheet
   */
-  preFiles: [],
+  prependedFiles: ['test/test.scss'],
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
-const Sass = require('node-sass');
+const fs = require('fs');
 const CSSNano = require('cssnano');
 const Gonzales = require('gonzales');
-const SweatMap = require('sweatmap');
+//const SweatMap = require('sweatmap');
 const config = require('../../PreStyleConfig');
+
+//Process our syntax
+const Adapter =
+  typeof config.adapter == 'function'
+  ? config.adapter
+  : require(config.adapter);
 
 const CSSNanoConfig = {
   autoprefixer: false,
@@ -13,21 +19,49 @@ const CSSNanoConfig = {
   discardDuplicates: true,
   discardEmpty: true,
   discardOverridden: true,
+  discardUnused: false,
+  filterOptimiser: true,
+  functionOptimiser: true,
+  mergeIdents: false,
+  mergeLonghand: true,
+  mergeRules: false,
+  minifyFontValues: true,
+  minifyGradients: true,
+  minifyParams: true,
+  minifySelectors: true,
+  normalizeCharset: false,
+  normalizeString: true,
+  normalizeUnicode: true,
+  normalizeUrl: true,
+  orderedValues: true,
+  reduceBackgroundRepeat: true,
+  reduceDisplayValues: true,
+  reduceIdents: false,
+  reduceInitial: false,
+  reducePositions: true,
+  reduceTimingFunctions: true,
+  reduceTransforms: true,
+  uniqueSelectors: true,
+  zindex: false,
 };
 
 module.exports = function PreStyle(cssstr) {
-  const prestr = '';
-  const sassObj = Sass.renderSync({
-    data: `${prestr} .PLACEHOLDER { ${cssstr.toString()} }`,
-    outputStyle: 'compact',
-  });
+  //Get all of the prependedFiles and string them together
+  const prestr = config.prependedFiles.map(fn => fs.readFileSync(fn).toString()).join('');
 
-  //First, process CSS via CSS Nano Config
-  CSSNano.process(sassObj.css.toString(), CSSNanoConfig)
+  //Use the adapater (by default Node-Sass) to process the statement
+  Adapter(`${prestr} .PLACEHOLDER { ${cssstr.toString()} }`)
+
+    //Next let's process it via CSS Nano
+    .then(cssObj => CSSNano.process(cssObj.toString(), CSSNanoConfig))
+
+    //Then let's run our CSS through the AST
     .then((result) => {
       const AST = Gonzales.srcToCSSP(result.css);
 
       console.log( result.css );
       console.log( Gonzales.csspToTree(AST) );
-    });
+    })
+
+    .catch(console.error);
 };
