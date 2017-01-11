1f32dda0b5752de81fd65a074a1f001221be0e79
Ben Solum <benjamin@soluml.com>
2017-01-11 11:24:29 -0600

    Can now seek out ruleset’s in ASTs.  These need to be mutated to produce atomic classes.

--- src/js/atomize.js
+++ src/js/atomize.js
@@ -4,7 +4,25 @@ module.exports = function Atomize(cssObj, PLACEHOLDER) {
   const CSS = cssObj.css;
   const AST = Gonzales.srcToCSSP(CSS);
 
-  console.log(PLACEHOLDER);
+  function processRuleset(ruleset) {
+    console.log(ruleset[0], Gonzales.csspToSrc(ruleset));
+  }
+
+  function seekOutRuleset(token) {
+    if (token) {
+      let i = 0;
+
+      while (token[i]) {
+        if (token[i] == 'ruleset') processRuleset(token);
+        else seekOutRuleset(token[i + 1]);
+        i++;
+      }
+    }
+  }
+
+  AST.forEach(seekOutRuleset);
+
+  console.log('');
   console.log(CSS);
   console.log('');
   console.log( Gonzales.csspToTree(AST) );
