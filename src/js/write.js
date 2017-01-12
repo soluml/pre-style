2aef562eaeab6c7aba6a3e5d67479eaf283e955f
Ben Solum <benjamin@soluml.com>
2017-01-12 12:56:36 -0600

    Need to pack media queries

--- src/js/write.js
+++ src/js/write.js
@@ -1,10 +1,19 @@
-const CSSNano = require('cssnano');
+const Gonzales = require('gonzales');
+const Normalize = require('./normalize');
 
 module.exports = function Write(cssObj) {
-  const className = cssObj.map(cls => cls.className).join(' ');
+  const classNames = cssObj.map(cls => cls.className).join(' ');
+  const uncss = Gonzales.csspToSrc(['stylesheet'].concat(cssObj.map(cls => cls.ast)));
 
-  console.log('------------');
-  console.log(className);
-  console.log(cssObj);
-  console.log('------------');
+  return new Promise((resolve, reject) => {
+    Normalize(uncss)
+      .then((data) => {
+        const { css } = data[0];
+
+        //Write stuff to file
+
+        //Resolve with this
+        resolve({ css, classNames });
+      }, reject);
+  });
 };
--- test/index.js
+++ test/index.js
@@ -2,44 +2,46 @@ const expect = require('expect');
 const PreStyle = require('../src/js/prestyle');
 
 describe('Pre-Style', () => {
-  it('Default character limits:', () => {
-    expect(
-      PreStyle`
-        /* TEST */
+  it('Default character limits:', (done) => {
+    PreStyle`
+      /* TEST */
 
-        font-weight: bold;
-        color: $color;
-        font-weight: bold;
-        font-size: .9em;
+      font-weight: bold;
+      color: $color;
+      font-weight: bold;
+      font-size: .9em;
 
-        @media (max-width: 600px) {
-          font-weight: normal;
-        }
+      @media (max-width: 600px) {
+        font-weight: normal;
+      }
 
-        @supports not ((text-align-last:justify) or (-moz-text-align-last:justify)) {
-          text-align: center;
-        }
+      @supports not ((text-align-last:justify) or (-moz-text-align-last:justify)) {
+        text-align: center;
+      }
 
-        @media (max-width: 600px) {
-          height: 30px;
-          font-size: 0.9em;
-          color: rgba(255,255,255,.3)
-        }
+      @media (max-width: 600px) {
+        height: 30px;
+        font-size: 0.9em;
+        color: rgba(255,255,255,.3)
+      }
 
-        @media (max-width: 600px), (min-width: 800px) {
-          background-color: yellow;
-          font-weight: normal;
-        }
+      @media (max-width: 600px), (min-width: 800px) {
+        background-color: yellow;
+        font-weight: normal;
+      }
 
-        .AnotherClass > & {
-          color: white!important;
-          font-weight: bolder;
-        }
+      .AnotherClass > & {
+        color: white!important;
+        font-weight: bolder;
+      }
 
-        &:hover {
-          color: white;
-        }
-      `
-    ).toBe(undefined);
+      &:hover {
+        color: white;
+      }
+    `.then((data) => {
+      console.log(data.css);
+      expect(data.classNames).toBe('A B C D E F G H I J K L M');
+      done();
+    });
   });
 });
