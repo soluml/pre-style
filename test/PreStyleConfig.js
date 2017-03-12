914796d26ca3c88d9b3074135d7bed95e715b22e
Ben Solum <benjamin@soluml.com>
2017-03-11 21:00:43 -0600

    spelling fix

--- test/PreStyleConfig.js
+++ test/PreStyleConfig.js
@@ -1,4 +1,4 @@
-//Partial configs are merged into the global config which contains the default adapater.
+//Partial configs are merged into the global config which contains the default adapter.
 module.exports = {
   prependedFiles: ['test/test.scss'],
 };
