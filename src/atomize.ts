ec7556ad74d424561437fc022d1ec666d4834a12
soluml <benjamin@soluml.com>
2021-09-17 15:49:47 -0500

    cleanup

--- src/atomize.ts
+++ src/atomize.ts
@@ -1,24 +1,27 @@
 import type PreStyle from './';
 import csstree from 'css-tree';
 
+interface AST {
+  type: 'StyleSheet';
+  loc: csstree.CssLocation|null;
+  children: (csstree.Atrule|csstree.Rule)[];
+}
+
 export default function Atomize(this: PreStyle, normalizedCss: string) {
-  const ast = csstree.parse(normalizedCss);
+  const ast = csstree.parse(normalizedCss) as any as AST;
+  const atomizedAst = {
+    type: "StyleSheet",
+    loc: null,
+    children: []
+  };
 
 
 
-  console.log(JSON.stringify(csstree.parse(`
-  .extraClassFilter,
-  .✝️ⓈⓞⓛⓘⒹⓔⓞⒼⓛⓞⓡⓘⓐ✝️ {
-    column-count: 5
-  }
-  `), null, 2))
+
+  
   
 
-  csstree.walk(ast, {
-    enter(node: csstree.CssNode) {
-      // console.log({node});
-    }
-  });
+  
 
 
 
