5d182f44cfa860e0c58528a41bee5a8550a18b7e
soluml <benjamin@soluml.com>
2021-09-17 16:27:22 -0500

    Atomizing Rule

--- src/atomize.ts
+++ src/atomize.ts
@@ -7,13 +7,69 @@ interface AST {
   children: (csstree.Atrule|csstree.Rule)[];
 }
 
+const deepClone = (obj: object) => JSON.parse(JSON.stringify(obj));
+
+
 export default function Atomize(this: PreStyle, normalizedCss: string) {
   const ast = csstree.parse(normalizedCss) as any as AST;
-  const atomizedAst = {
+  const atomizedAst: AST = {
     type: "StyleSheet",
     loc: null,
     children: []
   };
+  const placeholderFound = (obj: object) => !!~JSON.stringify(obj).indexOf(this.placeholder);
+
+
+
+
+  //
+  const fakeAst = csstree.parse(`
+    .extraClassFilter,
+    .✝️ⓈⓞⓛⓘⒹⓔⓞⒼⓛⓞⓡⓘⓐ✝️ {
+      column-count: 5
+    }
+  `) as any as AST;
+  //
+
+
+
+
+  function processRule(rule: csstree.Rule) {
+    // Create a new atom class for each selector piece
+    const prelude = rule.prelude as any as csstree.AtrulePrelude;
+    const arr: any[] = [];
+
+    prelude.children.forEach((pc) => {
+      const clone = deepClone(rule);
+
+      clone.prelude.children = [pc];
+      placeholderFound(clone) && arr.push(clone);
+    });
+
+    return arr;
+  }
+
+  // Process AST children
+  fakeAst.children.forEach((child) => {
+    switch(child.type) {
+      case 'Rule':
+        atomizedAst.children.push(processRule(child) as any);
+        break;
+    }
+  });
+
+  // Flatten a single level
+  atomizedAst.children = atomizedAst.children.flat();
+
+
+
+
+  //TODO: support @atrules
+
+
+  console.log({atomizedAst: csstree.generate(atomizedAst as any as csstree.CssNode)});
+
+
 
 
 
