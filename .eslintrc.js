3e59564ca37fa7cbc8de92421f9e2d265e63d7fd
Benjamin Solum <benjamin@soluml.com>
2021-09-19 14:55:29 -0500

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'airbnb',
+    'airbnb-base',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:jest/recommended',
     'prettier',
