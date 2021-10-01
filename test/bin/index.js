bfab3ca6f8d16c4dbc54ce8f8415d652f96bb689
soluml <benjamin@soluml.com>
2021-10-01 16:50:07 -0500

    Update tests

--- test/bin/index.js
+++ test/bin/index.js
@@ -9,8 +9,35 @@ function cli(args) {
 }
 
 describe('Bin', () => {
+  const configPath = 'test/bin/prestyle.config.json';
+
+  it('Fails with no config file', async () => {
+    const config = 'test/bin/bob.config.json';
+    const result = cli(['-c', config]);
+    const stdout = result.stdout.toString();
+    const stderr = result.stderr.toString();
+
+    expect(stderr).toBe('');
+    expect(
+      stdout.includes('Error: Cannot find module') && stdout.includes(config)
+    ).toBe(true);
+  });
+
+  it('Fails with no destination', async () => {
+    const result = cli(['-c', configPath]);
+    const stdout = result.stdout.toString();
+    const stderr = result.stderr.toString();
+
+    expect(stderr).toBe('');
+    expect(
+      stdout.includes(
+        'Error: You MUST specify a destination with -d or --destination.'
+      )
+    ).toBe(true);
+  });
+
   it('Gets the options', async () => {
-    const result = cli(['-c', 'test/bin/prestyle.config.js']);
+    const result = cli(['-c', configPath]);
     const stdout = result.stdout.toString();
     const stderr = result.stderr.toString();
 
