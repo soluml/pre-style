4200d7715d6e591fe264274c651655aa5cc9b818
soluml <benjamin@soluml.com>
2021-10-14 11:13:52 -0500

    Update config

---
+++ www/examples/@babel/config-overrides.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const {override, addBabelPlugins, addWebpackAlias} = require('customize-cra');
+
+const DIST_PATH = path.resolve(__dirname, '../../../dist');
+
+module.exports = override(
+  addBabelPlugins([
+    path.resolve(DIST_PATH, '@babel'), // In a real project, the plugin path is just `pre-style/@babel`
+    {importAsCSS: true},
+  ]),
+  // We need an alias to use the local `dist` as a node_module. In a real project, this line can be removed
+  addWebpackAlias({'pre-style$': path.resolve(DIST_PATH, 'src/index.js')})
+);
