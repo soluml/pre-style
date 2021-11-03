7e8ca2ba5ffe0b2aac2bcbedab81008921ba7c2e
soluml <benjamin@soluml.com>
2021-11-03 11:38:57 -0500

    Making updates

--- postcss/index.ts
+++ postcss/index.ts
@@ -1,20 +1,33 @@
 import type {Config} from 'global';
-import type * as postcss from 'postcss';
+import type * as postcssType from 'postcss';
+import postcss from 'postcss';
 import defaultConfig from '../bin/utils/defaultConfig';
 import PreStyle from '../src';
 
-module.exports = (config: Config): postcss.Plugin => {
+module.exports = (config: Config): postcssType.Plugin => {
   const PS = new PreStyle({
     ...defaultConfig,
     ...config,
   });
 
+  async function doRule(rule: postcssType.Node) {
+    if (rule.parent?.type !== 'root') return;
+
+    const css = rule.toString();
+    const {classNames} = await PS.process(css);
+
+    console.log(rule.type, {css, classNames});
+  }
+
   return {
     postcssPlugin: 'pre-style',
+    AtRule: doRule,
+    Rule: doRule,
     async OnceExit(css, {result}) {
       // Calls once per file, since every file has single Root
 
-      console.log({css, result});
+      // console.log({css, result});
+      console.log('EXIT');
     },
   };
 };
